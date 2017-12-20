20e77c9cd4058dd032b8b884cf0f392b48c9835b
Manuel <manuepeva@gmail.com>
2017-12-20 11:29:55 -0500

    route accept headers

---
+++ WebApplication1/Models/StudentsV2.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class StudentsV2
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
