6231b4f5b9ad58328f9eb84666a305fb4967f7db
Manuel <manuepeva@gmail.com>
2017-12-20 20:40:41 -0500

    routing headers in asp.net web app

---
+++ WebAPI2/Controllers/StudentV2Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    public class StudentV2Controller : ApiController
+    {
+        static List<StudentV2> students = new List<StudentV2>()
+       {
+           new StudentV2() {Id = 1, FirstName = "Lucho", LastName = "Quedaza", Gender = "Male", City = "Punot"},
+           new StudentV2() {Id = 2, FirstName = "Lucho", LastName = "Quedaza", Gender = "Male", City = "Punot" },
+           new StudentV2() {Id = 3, FirstName = "Lucho", LastName = "Quedaza", Gender = "Male", City = "Punot"},
+       };
+
+        public IEnumerable<StudentV2> Get()
+        {
+            return students;
+        }
+
+        public StudentV2 Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
---
+++ WebAPI2/Models/StudentV2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI2.Models
+{
+    public class StudentV2
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
